ca3e53ba1f549250e84f156955d1e727fe5d57ec
Patrik Novak <novakpatrik.99@protonmail.com>
2024-05-10 15:56:06 +0200

    Add script that generates gcode from sample svg files.

---
+++ generate_samples.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Ensure that gcode directory exists
+mkdir -p gcode
+
+# CVUT sample
+vpype --config vpype_config.toml \
+read svg/cvut.svg \
+scale -o 0 0 2 2 \
+linesort \
+gwrite -p xyplotter gcode/cvut.gcode
+
+# Flower sample
+vpype --config vpype_config.toml \
+read svg/flower.svg \
+linesort \
+gwrite -p xyplotter gcode/flower.gcode
+
+# Maker Faire sample
+vpype --config vpype_config.toml \
+read svg/mfprague-logo.svg \
+linesort \
+scale -o 0 0 0.5 0.5 \
+translate 20mm 20mm \
+gwrite -p xyplotter gcode/mfprague-logo.gcode
+
+# Benchmark sample
+./generate_test.sh
